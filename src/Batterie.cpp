3ed493726fe93ad558b9ccb8342f3b7bd2a01284
Geekoid85 <geekoid85@gmail.com>
2021-12-17 10:23:48 +0100

    Jauge de batterie OK

--- src/Batterie.cpp
+++ src/Batterie.cpp
@@ -1,11 +1,13 @@
 #include "Batterie.h"
 int nouveauNiveauBatterie = 74;
+LiFuelGauge gauge(MAX17043);
 int getNiveauBatterie() { // Je défini ma fonction
-    if (Serial.available() > 1) { // >1 et pas l'habituel >0 car sinon lors de la prochaine boucle il met 0 dans "niveauBatterie"
+    /*if (Serial.available() > 1) { // >1 et pas l'habituel >0 car sinon lors de la prochaine boucle il met 0 dans "niveauBatterie"
         nouveauNiveauBatterie = Serial.parseInt(); // Et pas Serial.read qui renverrait le numéro du caractère ascii
         Serial.print("niveau de batterie: ");
         Serial.print(nouveauNiveauBatterie);
         Serial.println(" %");
-    }
+    }*/
+    nouveauNiveauBatterie = gauge.getSOC();
     return nouveauNiveauBatterie;
 }
--- src/Batterie.h
+++ src/Batterie.h
@@ -1,6 +1,8 @@
 #ifndef BATTERIE_H
 #define BATTERIE_H
 #include <Arduino.h>
+#include <Wire.h>
+#include <LiFuelGauge.h>
 
 int getNiveauBatterie(); // Je déclare ma fonction
 
